ed2f96d26bed24e3c3a89c7283c8748ddb13b395
kautham <kauthamthiyagarajan@gmail.com>
2025-04-12 00:26:23 +0530

    #15 product server cross origin changes

--- product-service/src/main/java/com/ecommerce/product_service/config/WebConfig.java
+++ product-service/src/main/java/com/ecommerce/product_service/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://neocart-ui.onrender.com/")
                 .allowedMethods("*")
                 .allowedHeaders("*") // <--- this is key
                 .allowCredentials(true); // optional, use if you're using cookies or sessions
