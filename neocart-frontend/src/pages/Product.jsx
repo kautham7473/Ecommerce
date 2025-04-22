8ed856d3b1f3e671cc922485a496ba0d45f0ae2d
kautham <kauthamthiyagarajan@gmail.com>
2025-04-22 13:37:05 +0530

    #18 Register in Eureka

--- neocart-frontend/src/pages/Product.jsx
+++ neocart-frontend/src/pages/Product.jsx
@@ -62,7 +62,7 @@ const ProductPage = () => {
 
   const addToCart = async (userId, productId, quantity) => {
     try {
-      const response = await axios.post('${import.meta.env.VITE_CART_SERVICE_BASE_URL}/api/cart', {
+      const response = await axios.post(`${import.meta.env.VITE_CART_SERVICE_BASE_URL}/api/cart`, {
         userId,
         productId,
         quantity
